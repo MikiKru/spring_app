17ad2937eeb1b640a7e28320752715d71c6dbbb7
MikiKru <michal_kruczkowski@o2.pl>
2019-11-20 13:27:49 +0100

    spring_app v5

--- src/main/java/net/atos/spring_webapp/repository/UserRepository.java
+++ src/main/java/net/atos/spring_webapp/repository/UserRepository.java
@@ -3,9 +3,14 @@ package net.atos.spring_webapp.repository;
 import net.atos.spring_webapp.model.User;
 import org.springframework.data.domain.Sort;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 import java.util.List;
 @Repository
 public interface UserRepository extends JpaRepository<User, Long> {
+    @Query(
+            value = "select permission_id, count(*) from user_permission group by permission_id",
+            nativeQuery = true)
+    List<Object[]> aggregatePermissions();
 
 }
