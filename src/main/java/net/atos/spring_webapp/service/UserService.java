f27b23cb57c1f99d498e18729664564cb7e2b65d
MikiKru <michal_kruczkowski@o2.pl>
2019-11-21 13:15:36 +0100

    thymeleaf v12

--- src/main/java/net/atos/spring_webapp/service/UserService.java
+++ src/main/java/net/atos/spring_webapp/service/UserService.java
@@ -1,10 +1,13 @@
 package net.atos.spring_webapp.service;
 
+import net.atos.spring_webapp.model.Permission;
 import net.atos.spring_webapp.model.User;
 import net.atos.spring_webapp.repository.PermissionRepository;
 import net.atos.spring_webapp.repository.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import java.util.Optional;
 
 @Service
 public class UserService {
@@ -15,12 +18,14 @@ public class UserService {
         this.userRepository = userRepository;
         this.permissionRepository = permissionRepository;
     }
-    public User assignPermissionToUser(User user, byte roleId){
-        user
-        return
+    public User assignPermissionToUser(User user, int roleId){
+        Optional<Permission> permission = permissionRepository.findById((byte) roleId);
+        if (permission.isPresent()) {
+            user.addPermission(permission.get());
+        }
+        return user;
     }
-
     public void registerUser(User user){
-
+        userRepository.save(assignPermissionToUser(user, 1));
     }
 }
