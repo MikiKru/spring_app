bad0ea6383f22e27d6340b416195b601bd5672c2
MikiKru <michal_kruczkowski@o2.pl>
2019-11-21 13:22:58 +0100

    thymeleaf v13

--- src/main/java/net/atos/spring_webapp/controller/UserController.java
+++ src/main/java/net/atos/spring_webapp/controller/UserController.java
@@ -1,7 +1,36 @@
 package net.atos.spring_webapp.controller;
 
+import net.atos.spring_webapp.model.User;
+import net.atos.spring_webapp.service.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
+
+import javax.validation.Valid;
 
 @Controller
 public class UserController {
+    private UserService userService;
+    @Autowired
+    public UserController(UserService userService) {
+        this.userService = userService;
+    }
+    @GetMapping("/register")
+    public String registerUser(Model model){
+        model.addAttribute("user", new User());
+        return "register";
+    }
+    @PostMapping("/register")
+    public String registerUser(@ModelAttribute @Valid User user, BindingResult bindingResult){
+        if(bindingResult.hasErrors()){
+            return "register";
+        }
+        userService.registerUser(user); // automatycznie przypisujemy w metodzie uprawnienia ROLE_USER
+        return "redirect:/";
+    }
+
 }
