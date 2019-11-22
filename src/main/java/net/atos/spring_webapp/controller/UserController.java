f2d5fdadce994ae74fbdd2f497a1009e4c275816
MikiKru <michal_kruczkowski@o2.pl>
2019-11-22 13:53:03 +0100

    admin delete v20

--- src/main/java/net/atos/spring_webapp/controller/UserController.java
+++ src/main/java/net/atos/spring_webapp/controller/UserController.java
@@ -9,6 +9,7 @@ import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 import javax.validation.Valid;
 import java.security.Principal;
@@ -38,6 +39,7 @@ public class UserController {
         return "login";
     }
 
+    @ResponseBody
     @GetMapping("/user")
     public Principal loggedUser(Principal principal){
         return principal;
