1de9e2a8772ab5dfa6d54f03baf2339a383f588b
MikiKru <michal_kruczkowski@o2.pl>
2019-11-22 12:48:48 +0100

    admin delete v17

--- src/main/java/net/atos/spring_webapp/configuration/LangConfig.java
+++ src/main/java/net/atos/spring_webapp/configuration/LangConfig.java
@@ -1,5 +1,4 @@
 package net.atos.spring_webapp.configuration;
-
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
